72c2be3d1e2ca198b082142f5a40540e00b41973
Sadie Darbro <sdarbro1201@gmail.com>
2022-08-16 13:37:32 -0400

    Initial Commit

---
+++ To Do List Project/To Do.swift
@@ -0,0 +1,14 @@
+//
+//  To Do.swift
+//  To Do List Project
+//
+//  Created by                       Sadie Darbro                                                                   on 8/16/22.
+//
+
+
+import UIKit
+
+class ToDo {
+  var name = ""
+var importance = false
+}
